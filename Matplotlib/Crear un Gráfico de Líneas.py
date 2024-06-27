e158dae5827c74f76de3f11cc2856aa7e23e26dd
jponsgar <jponsgar@gmail.com>
2024-06-27 13:04:51 +0200

    Matplotlib

---
+++ Matplotlib/Crear un Gráfico de Líneas.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+# Datos de ejemplo
+x = [1, 2, 3, 4, 5]
+y = [2, 3, 5, 7, 11]
+
+# Crear el gráfico de líneas
+plt.plot(x, y)
+
+# Añadir título y etiquetas
+plt.title('Gráfico de Líneas')
+plt.xlabel('X')
+plt.ylabel('Y')
+
+# Mostrar el gráfico
+plt.show()
+
+# gráfico de líneas.PNG
--- Pandas/Escritura de Datos a un Archivo CSV.py
+++ Pandas/Escritura de Datos a un Archivo CSV.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-df = pd.read_csv('C:/Users/jpons/Documents/Home/Proveidors/Inkor/Modulo1/python/libs/archivo.csv')
+df = pd.read_csv('C:/Users/jpons/Code/Ejercicios/Python/Pandas/archivo.csv')
 # Suponiendo que df es un DataFrame
-df.to_csv('C:/Users/jpons/Documents/Home/Proveidors/Inkor/Modulo1/python/libs/archivo.csv', index=False)
+df.to_csv('C:/Users/jpons/Code/Ejercicios/Python/Pandas/archivo.csv', index=False)
